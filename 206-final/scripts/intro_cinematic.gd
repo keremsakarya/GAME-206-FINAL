075c5b4b862c51c1f67dd8c1f41d51b279ca907c
Kerem Sakarya <sakaryakerem252@gmail.com>
2026-05-16 15:09:27 +0300

    giriş sahnesi

---
+++ 206-final/scripts/intro_cinematic.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+@onready var intro_video = $IntroVideo
+@onready var continue_button = $ContinueButton
+
+@onready var player: CharacterBody3D = $"../Player"
+
+func _ready():
+	continue_button.hide()
+	
+	if player:
+		player.can_move = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	intro_video.play()
+
+func _on_intro_video_finished() -> void:
+	continue_button.show()
+
+
+func _on_continue_button_pressed() -> void:
+	if player:
+		player.can_move = true
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	queue_free()
