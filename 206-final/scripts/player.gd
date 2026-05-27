7b393bd5b8ce633147ac831a9a755f717696674d
ryucagan <dogancagan1208@gmail.com>
2026-05-28 00:50:52 +0300

    çim sesi colliderlar düşme sesi düşme screen shake i raytracing

--- 206-final/scripts/player.gd
+++ 206-final/scripts/player.gd
@@ -4,11 +4,15 @@ const WALK_SPEED = 3.0
 const SPRINT_SPEED = 5.5
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.002
+# --- CAMERA SHAKE & FOV VARIABLES ---
 const NORMAL_FOV = 75.0
-const SPRINT_FOV = 90.0 # Increase this to 100 for a more extreme speed effect
+const SPRINT_FOV = 90.0 
 var bob_time: float = 0.0
 const BOB_FREQ = 2.0
-const BOB_AMP = 0.06 # How aggressive the camera shake is
+const BOB_AMP = 0.06 
+
+var camera_base_y: float = 0.0
+var dip_tween: Tween
 # ------------------------------------
 
 var current_speed = WALK_SPEED
@@ -20,8 +24,11 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var can_move = true
 var can_exit = false
 @onready var anim_player = $Camera3D/Pistol/AnimationPlayer
+
 # --- AUDIO VARIABLES ---
 @onready var footstep_audio = $FootstepAudio
+@onready var landing_audio = $LandingAudio 
+@onready var grass_audio = $GrassAudio # <-- Added this back for you!
 @onready var floor_detector = $FloorDetector
 var step_timer: float = 0.0
 # -----------------------
@@ -30,10 +37,11 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pistol_model.visible = false
 	pickup_ui.hide()
+	
+	camera_base_y = camera.position.y
 
 # ----------- Fare Kontrolü (Etrafa Bakma) ----------
 func _unhandled_input(event):
-	# Fareyi boşa çıkarmak için ESC
 	if event.is_action_pressed("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -43,13 +51,8 @@ func _unhandled_input(event):
 	if not can_move: return
 	
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		# Sağa sola bakarken player da döner
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
-		
-		# Yukarı aşağı bakarken sadece kamera döner
 		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
-		
-		# Kamera takla atmasın diye açı sınırlaması
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 # ----------- Fizik - Hareket -----------
@@ -83,41 +86,62 @@ func _physics_process(delta):
 				anim_player.play("Pistol_RUN")
 			else:
 				anim_player.play("Pistol_WALK")
-	
 	else:
-		# Tuşları bırakınca tak diye durmasın
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
 		
 		if is_on_floor():
 			anim_player.play("Pistol_IDLE")
 	
+	# --------------------------------------
+	# 1. Check if we are in the air BEFORE we move
+	var was_in_air = not is_on_floor()
+	
 	move_and_slide() # This is your existing code
+	
+	# 2. Check if we hit the ground AFTER we move
+	if was_in_air and is_on_floor():
+		if floor_detector.is_colliding():
+			var ground = floor_detector.get_collider()
+			
+			if ground.is_in_group("House"):
+				landing_audio.pitch_scale = randf_range(0.9, 1.1)
+				landing_audio.volume_db = -8.0 
+				landing_audio.play(0.79) 
+			elif ground.is_in_group("Grass"):
+				# Soft dirt landing!
+				grass_audio.pitch_scale = randf_range(0.6, 0.7)
+				grass_audio.play()
+				
+			# --- CAMERA DIP TWEEN (Happens on all floors) ---
+			if dip_tween and dip_tween.is_valid():
+				dip_tween.kill() 
+			
+			dip_tween = create_tween()
+			dip_tween.tween_property(camera, "position:y", camera_base_y - 0.2, 0.1).set_trans(Tween.TRANS_SINE)
+			dip_tween.tween_property(camera, "position:y", camera_base_y, 0.25).set_trans(Tween.TRANS_SINE)
+			# ------------------------
+	# --------------------------------------
 	
 	# --- SPRINT EFFECTS (FOV & HEADBOB) ---
 	var target_fov = NORMAL_FOV
 	
-	# If we are moving, on the ground, and sprinting, trigger the speed effect
 	if direction and current_speed == SPRINT_SPEED and is_on_floor():
 		target_fov = SPRINT_FOV
 		
-	# Smoothly zoom the camera in and out for that "motion blur" speed feel
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	
-	# Camera Shake (Headbob)
 	if direction and is_on_floor():
-		# If sprinting, make the shake much more violent
 		var shake_multiplier = 1.5 if current_speed == SPRINT_SPEED else 1.0
 		bob_time += delta * BOB_FREQ * current_speed * shake_multiplier
 		
-		# v_offset and h_offset shake the camera lens without messing up mouse aiming!
 		camera.v_offset = sin(bob_time) * BOB_AMP * shake_multiplier
 		camera.h_offset = cos(bob_time * 0.5) * (BOB_AMP / 2.0) * shake_multiplier
 	else:
-		# Smoothly calm the camera down when the player stops moving
 		bob_time = 0.0
 		camera.v_offset = lerp(camera.v_offset, 0.0, delta * 5.0)
 		camera.h_offset = lerp(camera.h_offset, 0.0, delta * 5.0)
+	
 	# --------------------------------------
 	# --- FOOTSTEP AUDIO ---
 	if direction and is_on_floor():
@@ -126,20 +150,14 @@ func _physics_process(delta):
 			if floor_detector.is_colliding():
 				var ground = floor_detector.get_collider()
 				
-				# DEBUG 1: Tells us exactly what the laser hit
-				print("The laser hit an object named: ", ground.name)
-				
 				if ground.is_in_group("House"):
-					# DEBUG 2: Tells us the tag worked
-					print("House tag found! Playing sound!")
-					
 					footstep_audio.pitch_scale = randf_range(0.85, 1.15)
 					footstep_audio.play()
+				elif ground.is_in_group("Grass"):
+					grass_audio.pitch_scale = randf_range(0.85, 1.15)
+					grass_audio.play()
 				else:
-					# DEBUG 3: Tells us if the tag is missing
-					print("WARNING: This floor does NOT have the 'House' tag! Its current groups are: ", ground.get_groups())
-			else:
-				print("WARNING: Laser is floating and not hitting anything!")
+					print("WARNING: This floor does NOT have a tag! Its current groups are: ", ground.get_groups())
 			
 			if current_speed == SPRINT_SPEED:
 				step_timer = 0.3 
@@ -148,24 +166,18 @@ func _physics_process(delta):
 	else:
 		step_timer = 0.0
 	# ----------------------
-
+	
 # Silah tetiklenmesi
 func show_weapon_pickup_ui():
 	can_move = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	# Fade-in
 	var fade_target = $PickupUI/FadeTarget
-	fade_target.modulate.a = 0.0 # başta görünmezlik
+	fade_target.modulate.a = 0.0 
 	pickup_ui.show()
 	
-	# Alpha değerini 0.5 saniyede görünür yapma
 	var tween = create_tween()
 	tween.tween_property(fade_target, "modulate:a", 1.0, 0.5)
-
-# Butona basılınca
-
-
 
 func _on_button_pressed() -> void:
 	pickup_ui.hide()
