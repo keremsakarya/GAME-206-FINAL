87f94552a97f0ad9db3ac5129752b3c1ea132503
ryucagan <dogancagan1208@gmail.com>
2026-05-29 16:59:34 +0300

    sounds

--- 206-final/scripts/music_trigger.gd
+++ 206-final/scripts/music_trigger.gd
@@ -3,13 +3,18 @@ extends Area3D
 @onready var music_player = $AmbientPlayer
 var fade_tween: Tween
 
+# We will create a blank space to remember your custom volume
+var target_volume: float 
+
 func _ready() -> void:
-	# Wires are hard-coded!
+	# 1. The moment the game starts, memorize whatever volume you typed in the Inspector!
+	target_volume = music_player.volume_db
+	
+	# 2. Connect the wires
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node3D) -> void:
-	# BYPASSING GROUPS ENTIRELY: Just check the physical name!
 	if body.name == "Player":
 		
 		if fade_tween and fade_tween.is_valid():
@@ -18,8 +23,9 @@ func _on_body_entered(body: Node3D) -> void:
 		music_player.volume_db = -40.0 
 		music_player.play()
 		
+		# 3. Fade the music up to your memorized volume!
 		fade_tween = create_tween()
-		fade_tween.tween_property(music_player, "volume_db", 0.0, 2.5).set_trans(Tween.TRANS_SINE)
+		fade_tween.tween_property(music_player, "volume_db", target_volume, 2.5).set_trans(Tween.TRANS_SINE)
 
 func _on_body_exited(body: Node3D) -> void:
 	if body.name == "Player":
