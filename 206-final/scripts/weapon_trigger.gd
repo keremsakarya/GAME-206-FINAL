ff6a3c451d5ccd2651e47d415fc532b373ed3633
Kerem Sakarya <sakaryakerem252@gmail.com>
2026-06-04 19:43:18 +0300

    kapı kapanma

--- 206-final/scripts/weapon_trigger.gd
+++ 206-final/scripts/weapon_trigger.gd
@@ -1,7 +1,29 @@
 extends Area3D
 
-
 func _on_body_entered(body):
-	if body.name == "Player" and body.has_method("show_weapon_pickup_ui"):
-		body.show_weapon_pickup_ui()
+	# Sadece Player girdiğinde çalışsın
+	if body.name == "Player":
+		
+		# --- 0. SİLAH UI GÖSTERİMİ ---
+		if body.has_method("show_weapon_pickup_ui"):
+			body.show_weapon_pickup_ui()
+			
+		# --- 1. KAPIYI ANİMASYONLA DÖNDÜR ---
+		var kapilar = get_tree().get_nodes_in_group("HedefKapi")
+		for kapi in kapilar:
+			kapi.rotation_degrees.z = -180.0
+				
+		# --- 2. KİLİDİ GÖRÜNÜR YAP ---
+		var padlocks = get_tree().get_nodes_in_group("KirmiziKilit")
+		for red_pad in padlocks:
+			if red_pad:
+				red_pad.show() 
+				
+		# --- 3. ALTYAZI ALANINI UYANDIR ---
+		var pad_subs = get_tree().get_nodes_in_group("EvKilitAltyazi")
+		for red_sub in pad_subs:
+			if red_sub:
+				red_sub.process_mode = Node.PROCESS_MODE_INHERIT
+				
+		# --- 4. TETİKLEYİCİYİ SİL ---
 		queue_free()
